1b09828c01309a7050fae6de6921c7a680244a7d
Сергей Русинов <fftron@me.com>
2019-09-23 21:06:54 +0500

    Start

---
+++ Func/Counters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Func
+{
+    public class Counters
+    {
+        public static double Count(double a, double b, Func<double, double> f, double delta)
+        {
+            double x = (a + b) / 2;
+            double fa = f(a);
+            if (fa == 0) return a;
+
+            double fb = f(b);
+            if (fb == 0) return b;
+            if (fa * fb > 0) throw new Exception();
+            double fx = f(x);
+            while (Math.Abs(a -b) > delta && Math.Abs(fa - fb) > delta)
+            {
+                if (fx == 0) return x;
+                if(fa*fx < 0)
+                {
+                    b = x;
+                    fb = fx;
+                }
+                else
+                {
+                    a = x;
+                    fa = fx;
+                }
+                x = (a + b) / 2;
+                fx = f(x);                
+            }
+            fa = Math.Abs(fa);
+            fb = Math.Abs(fb);
+            fx = Math.Abs(fx);
+            if (fa < fb && fa < fx)
+            {
+                return a;
+            }
+            if (fb < fa && fb < fx)
+            {
+                return b;
+            }
+            return x;
+        }
+    }
+}
---
+++ Func/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> f = x => Math.Cos(x) + 2 * x - 1.5;
+            Console.Write("Start = ");
+            var start = double.Parse( Console.ReadLine());
+            Console.Write("End = ");
+            var end = double.Parse(Console.ReadLine());
+            Console.Write("Delta = ");
+            var delta = double.Parse(Console.ReadLine());
+            Console.WriteLine("x = ",Counters.Count(start, end,f,delta));
+        }
+    }
+}
